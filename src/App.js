1c980e039ea5adc30f148074b7e10c8d66ae4ca0
JamesTodhunter <90239716+JamesTodhunter@users.noreply.github.com>
2021-10-26 10:01:45 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+// import Header from './components/Webpage/Header';
+// import Footer from './components/Webpage/Footer';
+// import Sidebar from './components/Webpage/Sidebar';
+import Ticketmaster from './components/Ticketmaster';
+import Weather from './components/Weather';
+import Nasa from './components/Nasa';
 import './App.css';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      {/* <Header /> */}
+      {/* <Footer /> */}
+      {/* <Sidebar /> */}
+      <Ticketmaster />
+      <Weather />
+      <Nasa />
 
     </div>
-  );
+  )
 }
 
 export default App;
